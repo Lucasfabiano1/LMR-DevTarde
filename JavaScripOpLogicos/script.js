87c5d56b6cb50ca685bee8ccc2697ba72996b7bc
Lucasfabiano1 <123977243+Lucasfabiano1@users.noreply.github.com>
2023-05-19 19:58:00 +0000

    17h

--- JavaScripOpLogicos/script.js
+++ JavaScripOpLogicos/script.js
@@ -1,2 +1,10 @@
-let numero = document.getElementById("nDigitado").value;
-function nParImpar()
+function nParImpar() {
+    let numero = document.getElementById("nDigitado").value;
+    let verificar = parseInt(numero) % 2;
+    if (verificar == o) {
+        alert("o n° " + numero + " é par")
+    } else {
+        alert("o n° " + numero + " é impar sim")
+    }
+
+}
